73b6e8f4469c8d260455bb949dbc7e8f87292823
Jamaal Crichfield <jcrichfield2@gmail.com>
2018-05-30 23:17:19 -0400

    Completed Problem 3 for the Ruby w3resources excercises. Program prints a random number of elements

---
+++ prob3_select_random_elements.rb
@@ -0,0 +1,21 @@
+arr = [4, 3, 5, 5, 3, 66, 33, 134, 111]
+
+def select_random_elements(array_input)
+  rand_seed = Random.new
+  new_array = Array.new
+  selected_elements_count = rand_seed.rand(1..8)
+  $i = 0
+
+  while $i <= selected_elements_count - 1
+    new_array.push(array_input[$i])
+    $i += 1
+  end
+
+  puts "#{selected_elements_count} random elements from the array"
+  puts "[#{(new_array.join(' '))}]"
+
+end
+
+puts "Original array:"
+puts "[ #{(arr.join(' '))}]"
+select_random_elements(arr)
